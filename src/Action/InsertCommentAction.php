214cbd461f13cc14a8b04f84246fc17b01fea060
ilich2011 <ilya.shwetsow@yandex.ru>
2024-02-05 22:56:22 +0300

    Добавлена валидация запроса.

--- src/Action/InsertCommentAction.php
+++ src/Action/InsertCommentAction.php
@@ -3,21 +3,31 @@
 namespace App\Action;
 
 use App\Repositoty\CommentRepository;
+use App\Validator\InsertRequestValidator;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class InsertCommentAction
 {
     private CommentRepository $repository;
+    private InsertRequestValidator $validator;
 
-    public function __construct(CommentRepository $repository)
+    public function __construct(CommentRepository $repository, InsertRequestValidator $validator)
     {
+        $this->validator = $validator;
         $this->repository = $repository;
     }
 
     public function __invoke(Request $request, Response $response): Response
     {
-        $status = $this->repository->save($request->getParsedBody()['userName'], $request->getParsedBody()['content']);
+        $body = $request->getParsedBody();
+
+        if (!$this->validator->validateRequest($body)) {
+            $response->getBody()->write('invalid data');
+            return $response->withStatus(500);
+        }
+
+        $status = $this->repository->save($body['userName'], $body['content']);
         $response->getBody()->write(json_encode($status));
 
         return $response->withHeader('Content-Type', 'application/json')->withStatus(201);
