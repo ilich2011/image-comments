a339fb6d9475e4e94c52d62f37123a1afa35e7a9
ilich2011 <ilya.shwetsow@yandex.ru>
2024-02-06 22:23:50 +0300

    Инициализирован проект Vue

---
+++ vue/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  name: 'App',
+  components: {FooterComponent},
+}
+</script>
+
+<template>
+  <div>
+    Content will be here :/
+  </div>
+  <FooterComponent />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "FooterComponent"
+}
+</script>
+
+<template>
+<footer>
+  <span>Сайт был создан 06.02.2024</span>
+</footer>
+</template>
+
+<style scoped>
+
+</style>
